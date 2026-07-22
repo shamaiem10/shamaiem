eb9020c3dec68aa9d185c66973d46a5e4fbc4647
Shamaiem Shabbir <shamaiemshabbir14@gmail.com>
2026-07-23 02:17:11 +0500

    File added

---
+++ src/components/Availability.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Availability(){
+  return (
+    <section className="section">
+      <div className="container center">
+        <span className="lozenge live big"><span className="dot-live"/> Available</span>
+      </div>
+    </section>
+  )
+}
